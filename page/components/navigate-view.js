5eb66362840565e292c0c0b9c4e38ae99286c600
BillWang <76603338@qq.com>
2019-01-08 11:52:44 +0800

    路径为空的时候的判断

--- page/components/navigate-view.js
+++ page/components/navigate-view.js
@@ -7,16 +7,20 @@ Component({
     path: String
   },
   methods: {
-    onTap: function () {
+    onTap: function() {
       var that = this
-      console.log(that.properties)
-      var myEventDetail = {} // detail对象，提供给事件监听函数
-      var myEventOption = {} // 触发事件的选项
-      this.triggerEvent('myevent', myEventDetail, myEventOption)
+      if (that.properties.path != null && that.properties.path.length > 0) {
+        console.log(that.properties)
+        var myEventDetail = {} // detail对象，提供给事件监听函数
+        var myEventOption = {} // 触发事件的选项
+        this.triggerEvent('myevent', myEventDetail, myEventOption)
 
-      wx.navigateTo({
-        url: that.properties.path,
-      })
+        wx.navigateTo({
+          url: that.properties.path,
+        })
+      } else {
+        console.log("请传入需要跳转的地址")
+      }
     }
   }
 })
